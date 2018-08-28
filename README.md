5df06e81e58f707da75541ba8aea8f76dd256e16
Zhihao Jiang <jiang_zhi@worksap.co.jp>
2018-08-28 15:39:24 +0800

    add proxy settings

--- README.md
+++ README.md
@@ -4,11 +4,13 @@
 
 ## Build Setup
 
+copy `.config.js.example` to `.config.js` and make changes
+
 ``` bash
 # install dependencies
 npm install
 
-# serve with hot reload at localhost:8080
+# serve with hot reload at localhost:4000
 npm run dev
 
 # build for production with minification
@@ -17,5 +19,3 @@ npm run build
 # build for production and view the bundle analyzer report
 npm run build --report
 ```
-
-For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- config/index.js
+++ config/index.js
@@ -3,6 +3,12 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 
 const path = require('path')
+var cc = {}
+try {
+  cc = require('../.config')
+} catch (e) {
+  cc = require('../.config.js.example')
+}
 
 module.exports = {
   dev: {
@@ -10,11 +16,20 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+        target: cc.apiHost,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': cc.apiPrefix
+        }
+      }
+    },
+
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 4000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
