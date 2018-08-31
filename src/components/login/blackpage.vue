8a854d1c80c4461bebcf2de24eeb0b31c201d50a
yuan_t <yuan_t@worksap.co.jp>
2018-08-31 10:12:04 +0800

    #10 add connection among pages
    (cherry picked from commit 3178acb448325f42f83c16e67e0ec177f106ba5c)

--- src/components/login/blackpage.vue
+++ src/components/login/blackpage.vue
@@ -1,10 +1,13 @@
 <template>
-  <h1>Your account is still being reviewed, please wait.</h1>
+  <div class="black-container">
+    <h1>Your account is still being reviewed, please wait.</h1>
+    <router-link to="/login" style="font-size: 20px;">Log in </router-link>
+  </div>
 </template>
 <style lang="less" scoped>
-h1{
-  height: 80vh;
-  padding-top: 20vh;
-  text-align: center;
-}
+  .black-container{
+    height: 80vh;
+    padding-top: 20vh;
+    text-align: center;
+  }
 </style>
