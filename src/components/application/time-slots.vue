000ae278c75acf3585ad4af32e9db697ed956ee1
Zhihao Jiang <jiang_zhi@worksap.co.jp>
2018-09-19 16:56:45 +0800

    #59 generator for time periods

--- src/components/application/time-slots.vue
+++ src/components/application/time-slots.vue
@@ -11,12 +11,39 @@
       <a-select-option v-for="slot in slots" :key="slot">{{slot}}</a-select-option>
     </a-select>
     <div>
-      <a @click="restoreToDefaultSlots">Use Default</a>
+      <a @click="openGenerator">Generate</a>
       <a-divider type="vertical"/>
       <a @click="clearAllSlots">Clear All</a>
       <a-divider type="vertical"/>
       <span>Type format-like <a-tag>09:00 ~ 09:45</a-tag></span>
     </div>
+    <a-modal
+      title="Time Periods Generator"
+      :visible="generator.visible"
+      okText="Generate"
+      @ok="generate"
+      @cancel="closeGenerator"
+    >
+      <div style="margin-bottom: 16px">
+        <span>From</span>
+        <a-time-picker format="HH:mm" v-model="generator.from"/>
+        <span>To</span>
+        <a-time-picker format="HH:mm" v-model="generator.to"/>
+      </div>
+      <div style="margin-bottom: 16px">
+        <span>Estimate</span>
+        <a-input-number v-model="generator.duration"/>
+        <span>minutes for each interview</span>
+      </div>
+      <div style="margin-bottom: 16px">
+        <span>Interval</span>
+        <a-input-number v-model="generator.gap"/>
+        <span>minutes after each interview</span>
+      </div>
+      <div style="margin-bottom: 16px">
+        <a-checkbox v-model="generator.clear">Clear All</a-checkbox>
+      </div>
+    </a-modal>
   </div>
 </template>
 
@@ -32,7 +59,8 @@ export default {
   },
   data () {
     return {
-      slots: this.value
+      slots: this.value,
+      generator: {}
     }
   },
   watch: {
@@ -65,16 +93,30 @@ export default {
       })
       this.slots = slots.map(o => `${o.start.format('HH:mm')} ~ ${o.end.format('HH:mm')}`)
     },
-    restoreToDefaultSlots () {
-      const slots = []
-      for (const time = moment('2018-09-18 08:00'); time.isBefore(moment('2018-09-18 22:00'));) {
+    openGenerator () {
+      this.generator = {
+        visible: true,
+        from: moment('2018-09-19 08:00'),
+        to: moment('2018-09-19 22:00'),
+        duration: 45,
+        gap: 15,
+        clear: true
+      }
+    },
+    closeGenerator () {
+      this.generator.visible = false
+    },
+    generate () {
+      const slots = this.generator.clear ? [] : [...this.slots]
+      for (const time = this.generator.from; time.isBefore(this.generator.to);) {
         const start = time.format('HH:mm')
-        time.add(45, 'minute')
+        time.add(this.generator.duration, 'minute')
         const end = time.format('HH:mm')
-        time.add(15, 'minute')
+        time.add(this.generator.gap, 'minute')
         slots.push(`${start} ~ ${end}`)
       }
       this.slots = slots
+      this.closeGenerator()
     },
     clearAllSlots () {
       this.slots = []
